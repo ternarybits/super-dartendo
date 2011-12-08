17f4afd2f86fceaf5912980391e94df5b6c60795
yiuyuho <yiuyuho@755c7527-8e6f-264c-9c1e-12f10f8c8573>
2011-12-08 18:25:36 +0000

    Added Util.newIntList

--- dartendo/Util.dart
+++ dartendo/Util.dart
@@ -1,7 +1,18 @@
 class Util {
 
+  // System.arraycopy(rom.getRomBank(bank), 0, cpuMem.mem, address, 16384);
+  // arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
   void arraycopy(List<int> src, int srcPos, List<int> dest, int destPos, int length)
   {
+    // void setRange(int start, int length, List<E> from, [int startFrom])
     dest.setRange(destPos, length, src, srcPos);
+  }
+  
+  List<int> newIntList(int size, int defaultValue)
+  {
+    List<int> r = new List<int>(size);
+    for(int i = 0; i < size; ++i) r[i] = defaultvalue;
+    
+    return r;
   }
 }
